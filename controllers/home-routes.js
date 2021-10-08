3b16ffb7657a05f580ea76383c195dc9430b4a11
delaluz12 <75863526+delaluz12@users.noreply.github.com>
2021-10-08 14:35:01 -0500

    added routes for front-end functionality

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const { Post, User, Person } = require('../models');
-
+const { Post, User, Person, Neighborhood } = require('../models');
+const withAuth = require('../utils/auth')
 // GET all global posts for homepage
 router.get('/', async (req, res) => {
   try {
@@ -11,14 +11,14 @@ router.get('/', async (req, res) => {
       attributes: ['title', 'content', 'post_date_created'],
 
       include: [{
-          model: User,
-          attributes:['email'],
-          include: [{
-              model: Person,
-              attributes: ['first_name', 'last_name']
-          }],
-        }]
-  });
+        model: User,
+        attributes: ['email'],
+        include: [{
+          model: Person,
+          attributes: ['first_name', 'last_name']
+        }],
+      }]
+    });
     const posts = dbPostData.map((post) =>
       post.get({ plain: true })
     );
@@ -33,9 +33,20 @@ router.get('/', async (req, res) => {
 });
 
 // GET homepage - landing page with Join or Create
-router.get('/browse', (req, res) => {
-  res.render('browse');
-  return;
+router.get('/browse', async (req, res) => {
+
+  try {
+    //get neighborhoods to display in viewable list
+    const dbNeighborhoods = await Neighborhood.findAll();
+    const neighborhoods = dbNeighborhoods.map((neighborhood) =>
+      neighborhood.get({ plain: true })
+    );
+    res.render('browse', { neighborhoods });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
+
 });
 
 // GET login page
@@ -48,22 +59,53 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
-// GET signup page
-router.get('/signup', (req, res) => {
+// GET neighbor sign up FORM
+router.get('/admin', (req, res) => {
   if (req.session.loggedIn) {
     res.redirect('/');
     return;
   }
 
-  res.render('signup');
+  res.render('neighborhoodAdminForm');
+});
+
+// GET neighbor sign up FORM
+router.get('/neighbor', async (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+  //get neighborhoods to display on the drop-down in form
+  const dbNeighborhoods = await Neighborhood.findAll();
+  const neighborhoods = dbNeighborhoods.map((neighborhood) =>
+    neighborhood.get({ plain: true })
+  );
+  res.render('neighborForm', { neighborhoods });
 });
 
 //GET dashboard page
 router.get('/dashboard', (req, res) => {
   if (req.session.loggedIn) {
     res.render('dashboard');
-  return;
+    return;
   }
 });
+
+//GET person Form
+router.get('/person', withAuth, (req, res) => {
+  try {
+    // must be logged in to access person form
+    if (!req.session.loggedIn) {
+      res.redirect('/login')
+    }
+    res.render('personForm', { loggedIn: req.session.loggedIn })
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+
 
 module.exports = router;
