3591567ed92194e06e7301d860628d93441af1df
lvaillan <classvaillan@gmail.com>
2021-10-07 21:06:54 -0500

    heroku deployment

--- server.js
+++ server.js
@@ -35,6 +35,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
+// sequelize.sync({ force: false }).then(() => {
+//   app.listen(PORT, () => console.log('Now listening'));
+// });
+
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
