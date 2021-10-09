20695a4f2cae322386b393b9f1c4185a3eef7470
lvaillan <classvaillan@gmail.com>
2021-10-09 11:19:18 -0500

    dashboard post and household routes

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
-const { Person, Post, User, Unit, Neighborhood } = require('../models');
-const { restore } = require('../models/Event');
+const { Person, Post, User, Unit, Neighborhood, Event } = require('../models');
 // Import the custom middleware
 const withAuth = require('../utils/auth');
-// GET data for the dashboard
+
+// GET Post data for the dashboard
 router.get('/', withAuth, async (req, res) => {
   try {
     const dbPostData = await Post.findAll({
@@ -20,34 +20,34 @@ router.get('/', withAuth, async (req, res) => {
         },
         {
           model: Unit,
+          where: {
+            neighborhood_id: req.session.neighborhood_id
+          }
         }],
       }]
     });
+    console.log(dbPostData);
     const posts = dbPostData.map((post) =>
       post.get({ plain: true })
     );
-    const dbNeighborData = await Neighborhood.findAll({
-      where: {
-        // this is hardcoded for now until we know how it is coming from the webpage
-        neighborhood_id: 1
-      },
-      attribute: ['id'],
-      include: [{
-        model: Unit,
-        attributes: ['unit_number', 'unit_name'],
-        include: [{
-          model: Person,
-          attributes: ['first_name', 'last_name', 'type', 'phone', 'cell', 'birth_date']
-        }],
-      }]
+
+    res.render('dashboard', {
+      posts,
+      loggedIn: req.session.loggedIn,
     });
-    const neighbors = dbNeighborData.map((neighbors) =>
-      neighbors.get({ plain: true })
-    );
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// GET household data for the dashboard
+router.get('/household', async (req, res) => {
+  try {
+   
     const dbHouseholdData = await Unit.findAll({
       where: {
-        // this is hardcoded for now until we know how it is coming from the webpage
-        id: 1
+        neighborhood_id: 1
       },
       attribute: ['id'],
       include: [{
@@ -58,16 +58,16 @@ router.get('/', withAuth, async (req, res) => {
       household.get({ plain: true })
     );
     res.render('dashboard', {
-      posts,
-      neighbors,
-      household,
-      loggedIn: req.session.loggedIn, style: 'dashboard'
+    household, 
+      loggedIn: req.session.loggedIn,
     });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
   }
 });
+
+
 // GET all neighbors
 router.get('/neighbors', withAuth, async (req, res) => {
   try {
