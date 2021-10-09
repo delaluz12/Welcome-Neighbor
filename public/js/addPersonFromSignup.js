1468643059843a895c524edb38fd732cd0c009fb
delaluz12 <75863526+delaluz12@users.noreply.github.com>
2021-10-08 22:05:28 -0500

    script to add User profile data to Person table

--- public/js/addPersonFromSignup.js
+++ public/js/addPersonFromSignup.js
@@ -1,38 +1,49 @@
-const newPersonFromSignupFormHandler =  (event) => {
+const newPersonFromSignupFormHandler = (event) => {
     event.preventDefault();
     const formEl = document.getElementById('data');
-    console.log(formEl);
+    // console.log(formEl);
 
 
-    const user_id =formEl.getAttribute('data-user_id');
+    const user_id = formEl.getAttribute('data-user_id');
     const unit_id = formEl.getAttribute('data-unit_id');
 
     const first = document.getElementById('first_name').value.trim();
     const last = document.getElementById('last_name').value.trim();
+    // console.log(first);
+    // console.log(last);
+
 
     const e = document.getElementById('person_type');
     const type = e.options[e.selectedIndex].value;
-    const phone = document.getElementById('phone').value.trim();
-    const cell = document.getElementById('cell').value.trim();
+    const phoneEl = document.getElementById('phone').value.trim();
+    const phone=phoneEl.replace(/[^0-9]/g,'');
+    const cellEl = document.getElementById('cell').value.trim();
+    const cell=cellEl.replace(/[^0-9]/g,'');
     const birthday = document.getElementById('birthday').value.trim();
+    // console.log(type);
+    // console.log(phone);
+    // console.log(typeof phone);
+    // console.log(cell);
+    // console.log(typeof birthday);
 
 
-    console.log(unit_id);
-    console.log(user_id);
-    // if (email && password && role_id && name && city && state && zip && unit_number && street) {
-    //     const response = await fetch('/api/post/', {
-    //         method: 'POST',
-    //         body: JSON.stringify({ email, password, role_id, name, city, state, zip, unit_number, street }),
-    //         headers: { 'Content-Type': 'application/json' },
-    //     });
-    //     console.log(response);
+    // console.log(unit_id);
+    // console.log(user_id);
 
-    //     if (response.ok) {
-    //         document.location.replace('/');
-    //     } else {
-    //         alert('Failed to sign up.');
-    //     }
-    // }
+    if (first && last && type && phone && cell && birthday && user_id && unit_id) {
+        const response = await fetch('/api/post/', {
+            method: 'POST',
+            body: JSON.stringify({ first, last, type, phone, cell, birthday, user_id, unit_id }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+        console.log(response);
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert('Failed to sign up.');
+        }
+    }
 
 
 };
