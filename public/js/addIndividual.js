ae6e0bae07f7d15797e1202b126506d27d720f6f
lvaillan <classvaillan@gmail.com>
2021-10-11 13:14:43 -0500

    add person from profile

--- public/js/addIndividual.js
+++ public/js/addIndividual.js
@@ -8,6 +8,26 @@ const newPersonFromDashboard = async (event) => {
   const cell = document.querySelector('#cell').value;
   const birth_date = document.querySelector('#birthday').value;
 
+  if (first_name) {
+      const response = await fetch('/api/person/new', {
+          method: 'POST',
+          body: JSON.stringify({ first_name, last_name, person_type, phone, cell, birth_date }),
+          headers: { 'Content-Type': 'application/json' },
+      });
+      console.log(response);
+
+      if (response.ok) {
+          document.location.replace('/dashboard/household');
+      } else {
+          alert('Failed to add person.');
+      }
+  }
+};
+
+document.querySelector('#addIndForm').addEventListener('submit', newPersonFromDashboard);
+
+
+
 
   // const e = document.getElementById('person_type');
   // const person_type = e.options[e.selectedIndex].value;
@@ -25,60 +45,4 @@ const newPersonFromDashboard = async (event) => {
 
   // console.log(typeof unit_id);
   // console.log(typeof user_id);
-
-  if (first_name && last_name && person_type && phone && cell && birth_date) {
-      const response = await fetch('/api/person/new', {
-          method: 'POST',
-          body: JSON.stringify({ first_name, last_name, person_type, phone, cell, birth_date, unit_id }),
-          headers: { 'Content-Type': 'application/json' },
-      });
-      console.log(response);
-
-      if (response.ok) {
-          document.location.replace('/dashboard');
-      } else {
-          alert('Failed to sign up.');
-      }
-  }
-
-
-};
-
-document.querySelector('.addIndForm').addEventListener('submit', newPersonFromDashboard);
-
-// async function addInd(event) {
-//     event.preventDefault();
-
-//     const first_name = document.querySelector('#newIndF').value;
-//     const last_name = document.querySelector('#newIndL').value;
-//     const type = document.querySelector('#type').value;
-//     const phone = document.querySelector('#phone').value;
-//     const cell = document.querySelector('#cell').value;
-//     const birth_date = document.querySelector('#bd').value;
-//     const unit_id = req.session.unit_id;
-
-//     const response = await fetch(`/api/person/`, {
-//       method: 'POST',
-//       body: JSON.stringify({
-//         first_name,
-//         last_name,
-//         type,
-//         phone,
-//         cell,
-//         birth_date,
-//         unit_id
-//       }),
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//     });
-    
-//     if (response.ok) {
-//       document.location.replace('/');
-//     } else {
-//       alert('Failed to add new individual');
-//     }
-//   }
-  
-//   document.querySelector('.addIndForm').addEventListener('submit', addInd);
     
